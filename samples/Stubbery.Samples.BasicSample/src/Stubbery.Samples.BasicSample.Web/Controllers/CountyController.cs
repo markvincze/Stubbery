0db467f9ec7cf1b2e447de914fe8061e113911d6
Mark Vincze <mvincze@travix.com>
2016-06-11 23:40:58 +0200

    Add sample project

---
+++ samples/Stubbery.Samples.BasicSample/src/Stubbery.Samples.BasicSample.Web/Controllers/CountyController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Stubbery.Samples.BasicSample.Web.Controllers
+{
+    [Route("")]
+    public class CountyController : Controller
+    {
+        private readonly IConfiguration configuration;
+
+        public CountyController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [Route("countyname/{postcode}")]
+        public async Task<IActionResult> GetCountyName(string postcode)
+        {
+            using (var hc = new HttpClient())
+            {
+                var uri = new UriBuilder(configuration["PostCodeApiUrl"])
+                {
+                    Path = $"postcodes/{postcode}"
+                }.Uri;
+
+                var response = await hc.GetAsync(uri);
+
+                using(var stream = await response.Content.ReadAsStreamAsync())
+                using (var reader = new StreamReader(stream))
+                using (var jsonTextReader = new JsonTextReader(reader))
+                {
+                    var jObject = JObject.Load(jsonTextReader);
+
+                    if (jObject["status"].ToString() == "200")
+                    {
+                        return Ok(jObject["result"]["admin_county"].ToString());
+                    }
+
+                    if (jObject["status"].ToString() == "404")
+                    {
+                        return NotFound();
+                    }
+
+                    return StatusCode(500);
+                }
+            }
+        }
+    }
+}
---
+++ samples/Stubbery.Samples.BasicSample/test/Stubbery.Samples.BasicSample.IntegrationTests/HttpClientExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Stubbery.Samples.BasicSample.IntegrationTests
+{
+    public static class HttpClientExtensions
+    {
+        public static HttpClient AcceptJson(this HttpClient client)
+        {
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept
+                .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return client;
+        }
+    }
+}
