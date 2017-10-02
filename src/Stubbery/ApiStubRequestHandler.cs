286d7ee96c8de2fc9b53660867675fe55f64daed
Lucas Lara <lucaslorentzlara@hotmail.com>
2017-10-02 14:18:36 +0200

    Avoid connections hanging on linux

--- src/Stubbery/ApiStubRequestHandler.cs
+++ src/Stubbery/ApiStubRequestHandler.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Stubbery.RequestMatching;
+using System.IO;
 
 namespace Stubbery
 {
@@ -17,13 +18,21 @@ namespace Stubbery
 
         public async Task HandleAsync(HttpContext httpContext)
         {
-            httpContext.Response.StatusCode = StatusCodes.Status404NotFound;
-
-            var firstMatch = configuredEndpoints.FirstOrDefault(e => e.IsMatch(httpContext));
-
-            if (firstMatch != null)
+            using (var bodyStream = new MemoryStream())
             {
-                await firstMatch.SendResponseAsync(httpContext);
+                // Ensure the request body is fully read to avoid hanging connections on linux
+                await httpContext.Request.Body.CopyToAsync(bodyStream);
+                httpContext.Request.Body = bodyStream;
+                bodyStream.Position = 0;
+
+                httpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+
+                var firstMatch = configuredEndpoints.FirstOrDefault(e => e.IsMatch(httpContext));
+
+                if (firstMatch != null)
+                {
+                    await firstMatch.SendResponseAsync(httpContext);
+                }
             }
         }
     }
