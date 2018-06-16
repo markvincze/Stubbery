65cee4a2da7409f07ae4dac4e56d898cf8e438ae
Mark Vincze <mrk.vincze@gmail.com>
2018-06-16 23:38:09 +0200

    Set up automatically publishing the docs (#13)
    * Set up automatically publishing the docs
    
    * Only push docs on master

---
+++ releaseDocs.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -e
+
+export VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community"
+export VisualStudioVersion="15.0"
+
+docfx ./docs/docfx.json
+
+SOURCE_DIR=$PWD
+TEMP_REPO_DIR=$PWD/../my-project-gh-pages
+
+echo "Removing temporary doc directory $TEMP_REPO_DIR"
+rm -rf $TEMP_REPO_DIR
+mkdir $TEMP_REPO_DIR
+
+echo "Cloning the repo with the gh-pages branch"
+git clone https://github.com/markvincze/Stubbery --branch gh-pages $TEMP_REPO_DIR
+
+echo "Clear repo directory"
+cd $TEMP_REPO_DIR
+git rm -r *
+
+echo "Copy documentation into the repo"
+cp -r $SOURCE_DIR/docs/_site/* .
+
+if [ "$APPVEYOR_REPO_BRANCH" == "master" ]
+then
+    echo "Push the new docs to the remote branch"
+    git add . -A
+    git commit -m "Update generated documentation"
+    git push origin gh-pages
+else
+    echo "Not on master, skipping pushing docs"
+fi
